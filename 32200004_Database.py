9c0725d67d927c4773e08f8368f531a491cb552e
KyRzky <105965096+KyRzky@users.noreply.github.com>
2023-03-18 14:45:30 +0700

    Update 32200004_Database.py

--- 32200004_Database.py
+++ 32200004_Database.py
@@ -14,4 +14,4 @@ df = load_data(st.secrets["public_gsheets_url"])
 
 # Print results.
 for row in df.itertuples():
-    st.write(f"{row.Nama} has a :{row.Nilai}:")
+    st.write(f"{row.Nama} Mendapatkan Nilai :{row.Nilai}:")
