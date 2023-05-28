549dda47f64352478036e097b405d527a68284b5
KyRzky <105965096+KyRzky@users.noreply.github.com>
2023-05-28 19:47:37 +0700

    Update cbcsv.py

--- cbcsv.py
+++ cbcsv.py
@@ -5,12 +5,12 @@ sheet_id = "1I8yfLFfTQNscutI8kwY_bhrFCYNQxtWDJrLln9wdz0E"
 sheet_name = "Data"
 url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 
-df = pd.read_csv(url)
+df2020k2 = pd.read_csv(url)
 
-st.write(df)
+st.write(df2020k2)
 
 # Remove the 'Bulan' column from the DataFrame as it will be used as the index
-df.set_index('Bulan', inplace=True)
+df2020k2.set_index('Bulan', inplace=True)
 
 # Plot the line chart
-st.bar_chart(df)
+st.bar_chart(df2020k2)
